66d443c090cc66855a586db70e2bb656bd2dc91f
Nila Sisakyan <Nila_Sisakyan@epam.com>
2019-06-06 21:13:44 +0400

    Add JSS in project

--- src/components/Shared/Button/index.js
+++ src/components/Shared/Button/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import cs from 'classnames';
-import './index.css';
+
+import injectSheet from 'react-jss';
+
+const styles = {
+    myButton: {
+        border: 'none',
+        margin: '0 10px',
+    }
+}
 
 class Button extends React.Component { 
     render() {
+        let { classes } = this.props;
         return (
-            <button className={cs(this.props.className, "button")} onClick={this.props.onClick}>{this.props.name}</button>
+            <button className={cs(this.props.className, classes.myButton)} onClick={this.props.onClick}>{this.props.name}</button>
         );
     }
 }
 
-export default Button;
+export default injectSheet(styles)(Button);
